7a75256f129f39e29f1d2ae2acf4e8a9468f488e
Megan Leander <mleander@wisc.edu>
2016-01-15 13:22:17 -0600

    add assert to catch list errors

--- stats.py
+++ stats.py
@@ -1,8 +1,9 @@
 def mean(vals):
-    """Calculate the arithmetic mean of a list of numbers in vals"""
+    assert type(vals) is list, 'wrong imput format'
     total = sum(vals)
     length = len(vals)
     return total/length
 
 print(mean([2,4]))
 
+print(mean("hello"))
