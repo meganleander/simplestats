513cff96b7680f906db596d521da7996c1a48210
Megan Leander <mleander@wisc.edu>
2016-01-15 13:27:45 -0600

    add test of mean

--- stats.py
+++ stats.py
@@ -1,9 +1,13 @@
 def mean(vals):
     assert type(vals) is list, 'wrong imput format'
-    total = sum(vals)
+    
+	total = sum(vals)
     length = len(vals)
     return total/length
 
-print(mean([2,4]))
 
-print(mean("hello"))
+def test_mean():
+	assert mean([2,4]))==3.0
+test_mean()
+
+#print(mean("hello"))
