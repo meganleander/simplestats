7ddc561c4025816d4717591f980c2948cd3139b6
Megan Leander <mleander@wisc.edu>
2016-01-15 13:59:13 -0600

    add assert_equal functions

--- test_stats.py
+++ test_stats.py
@@ -1,13 +1,15 @@
 from stats import mean
+from nose.test import assert_equal
 
 def test_mean():
-	assert mean([2,4]) == 3.0
-test_mean()
+	assert_equal(mean([2,4]),3.0)
+#test_mean()
 
 def test_empty_list():
-	assert mean([]) == 0.0
-test_empty_list()
+	assert_equal(mean([]),0.0)
+#test_empty_list()
 
 def test_float():
-	assert mean([3.14,3.14]) == 3.14
-test_float()
+	assert mean([.2,.4]) == .3
+#test_float()
+
